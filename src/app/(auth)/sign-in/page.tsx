80f9f7bcc69ec98cd9c1cb231f9744da4f8f8369
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2025-12-18 17:26:28 +0530

    fix: add suspense boundaries

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,21 @@
 import SignInForm from "@/components/authentication/sign-in-form";
 import { AppIcon } from "@/components/icons";
+import { Suspense } from "react";
 
 export default function SignInPage() {
   return (
     <main className="mx-auto flex min-h-screen w-full max-w-3xl flex-col justify-center py-12 px-6 gap-10 sm:py-16 sm:px-8 items-center font-inter">
       <AppIcon />
-      <SignInForm />
+
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center min-h-screen w-fll">
+            <div className="loader" />
+          </div>
+        }
+      >
+        <SignInForm />
+      </Suspense>
     </main>
   );
 }
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,11 +1,20 @@
 import SignUpForm from "@/components/authentication/sign-up-form";
 import { AppIcon } from "@/components/icons";
+import { Suspense } from "react";
 
 export default function SignInPage() {
   return (
     <main className="mx-auto flex min-h-screen w-full max-w-3xl flex-col justify-center py-12 px-6 gap-10 sm:py-16 sm:px-8 items-center font-inter">
       <AppIcon />
-      <SignUpForm />
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center min-h-screen w-fll">
+            <div className="loader" />
+          </div>
+        }
+      >
+        <SignUpForm />
+      </Suspense>
     </main>
   );
 }
